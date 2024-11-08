2d9cd73061036104089242c4c2f5f1c1bd119f36
Jonathan Sao <124708158+JKrisnaSao@users.noreply.github.com>
2024-11-07 22:09:05 -0500

    Crud Api added and organized

---
+++ src/main/java/com/example/CRUD/API/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.CRUD.API;
+
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    UserEntity findByUserName(String userName); //Using for login validation
+}
